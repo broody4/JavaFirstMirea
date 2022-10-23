3d6744ef8848acc6d009931d8bb4b3cd9a0ec3ef
egor.matolygin <egormatolygin@gmail.com>
2022-10-23 15:39:01 +0300

    Исправил сортировку

--- src/ru/mirea/task9/SortByGPA.java
+++ src/ru/mirea/task9/SortByGPA.java
@@ -3,43 +3,28 @@ package ru.mirea.task9;
 import java.util.Scanner;
 
 public class SortByGPA  {
-    public void quickSort(Student[] source, int leftBorder, int rightBorder) {
+    public void quickSort(Student[] array, int left, int right) {
         StudentComparator comp = new StudentComparator();
-        int leftMarker = leftBorder;
-        int rightMarker = rightBorder;
-        Student pivot = source[(leftMarker + rightMarker) / 2];
-        do {
-            // Двигаем левый маркер слева направо пока элемент меньше, чем pivot
-            while (comp.compare( source[leftMarker], pivot)<0) {
-                leftMarker++;
-            }
-            // Двигаем правый маркер, пока элемент больше, чем pivot
-            while (comp.compare(source[rightMarker],pivot)>0) {
-                rightMarker--;
-            }
-            // Проверим, не нужно обменять местами элементы, на которые указывают маркеры
-            if (leftMarker <= rightMarker) {
-                // Левый маркер будет меньше правого только если мы должны выполнить swap
-                if (leftMarker < rightMarker) {
-                    Student tmp = source[leftMarker];
-                    source[leftMarker] = source[rightMarker];
-                    source[rightMarker] = tmp;
-                }
-                // Сдвигаем маркеры, чтобы получить новые границы
-                leftMarker++;
-                rightMarker--;
-            }
-        } while (leftMarker <= rightMarker);
+        if ( left >= right )
+            return;
 
-        // Выполняем рекурсивно для частей
-        if (leftMarker < rightBorder) {
-            quickSort(source, leftMarker, rightBorder);
-        }
-        if (leftBorder < rightMarker) {
-            quickSort(source, leftBorder, rightMarker);
-        }
+        Student tmp = array[left];
+        array[left] = array[(left+right)/2];
+        array[(left+right)/2] = tmp;
+        int last = left;
+        for ( int i = last + 1; i <= right; ++i )
+            if (comp.compare(array[i] , array[left] )>=0) {
+                last++;
+                tmp = array[last];
+                array[last] = array[i];
+                array[i] = tmp;
+            }
+        tmp = array[left];
+        array[left] = array[last];
+        array[last] = tmp;
+        quickSort(array, left, last - 1);
+        quickSort(array, last + 1, right);
     }
-
 
     public static void main(String [] args){
         System.out.println("Введите количество студентов");
